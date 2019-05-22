95bc27ecc92fd12c253d185c1e079cb140ed53bd
kellydelahunty <48736483+kellydelahunty@users.noreply.github.com>
2019-05-23 08:52:37 +1000

    DId stuff thaks

--- Project JS.js
+++ Project JS.js
@@ -1,1 +1,16 @@
 // yet to be started ( s o o n )
+var targetFirstName = document.getElementById("firstName").value;
+var targetLastName = document.getElementById("lastName").value;
+
+var userEntered = {
+  firstName: targetFirstName,
+  lastName: targetLastName,
+}
+
+document.getElementById("btnLogin").addEventListener("click", storeNames);
+
+function storeNames(){
+  localStorage.setItem('userEntered', JSON.stringify(userEntered));
+  var retrievedObject = localStorage.getItem('userEntered');
+  console.log('retrievedObject: ', JSON.parse(retrievedObject));
+}
