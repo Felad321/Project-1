bb1ec98a1855abe33f6eab9af2b649180bffbecf
Sam Martin <sammartin@SMac.local>
2019-05-23 09:18:37 +1000

    Started JS

--- Project JS.js
+++ Project JS.js
@@ -1,16 +1,29 @@
 // yet to be started ( s o o n )
-var targetFirstName = document.getElementById("firstName").value;
-var targetLastName = document.getElementById("lastName").value;
 
-var userEntered = {
-  firstName: targetFirstName,
-  lastName: targetLastName,
-}
 
-document.getElementById("btnLogin").addEventListener("click", storeNames);
+//document.getElementById("btnLogin").addEventListener("click", storeNames);
+
+var el = document.getElementById("btnLogin");
+    if(el){
+        el.addEventListener("click",storeNames(),false)
+    }
+
+
+    var userEntered = {
+        personFirstName: document.getElementById("targetFirstName"),
+        personLastName: document.getElementById("targetLastName"),
+      }
+      
 
 function storeNames(){
-  localStorage.setItem('userEntered', JSON.stringify(userEntered));
-  var retrievedObject = localStorage.getItem('userEntered');
-  console.log('retrievedObject: ', JSON.parse(retrievedObject));
-}
+    targetFirstName = document.getElementById("firstName").value;
+    targetLastName = document.getElementById("lastName").value;
+    localStorage.setItem('userEntered', JSON.stringify(userEntered));
+    var retrievedObject = localStorage.getItem('userEntered');
+    console.log('retrievedObject: ', JSON.parse(retrievedObject));
+    console.log(userEntered.personFirstName);
+    console.log(userEntered.personLastName);
+    }
+    
+
+
